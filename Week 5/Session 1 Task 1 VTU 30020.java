d0243f0d6938cb9aacbcc24eb9782c5c5c750c82
vtu30020 <vtu30020@veltech.edu.in>
2026-08-28 09:31:09 +0530

    Create Session 1 Task 1 VTU 30020.java

---
+++ Week 5/Session 1 Task 1 VTU 30020.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+}
+
+Output:
+nums = [-2,1,-3,4,-1,2,1,-5,4]
+6
